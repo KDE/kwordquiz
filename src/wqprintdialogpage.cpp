2c4ec99bdac50a9730d75966938a4ad54f41faf7
Peter Hedlund <peter@peterandlinda.com>
2004-02-22 00:06:43 +0000

    Custom dialog page for print dialog
    svn path=/trunk/kdeedu/kwordquiz/; revision=290100

---
+++ src/wqprintdialogpage.cpp
@@ -0,0 +1,70 @@
+//
+// C++ Implementation: wqprintdialogpage
+//
+// Description: 
+//
+//
+// Author: Peter Hedlund <peter@peterandlinda.com>, (C) 2004
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include <qbuttongroup.h>
+#include <qradiobutton.h>
+#include <qlayout.h>
+
+#include <klocale.h>
+
+#include "wqprintdialogpage.h"
+
+WQPrintDialogPage::WQPrintDialogPage(QWidget *parent, const char *name )
+ : KPrintDialogPage( parent, name )
+{
+  setTitle(i18n("Vocabulary Options"));
+  
+  QGridLayout * l = new QGridLayout( this, 1, 1, 11, 6);
+
+  g = new QButtonGroup(i18n("Select type of printout"), this );
+  g->setColumnLayout(0, Qt::Vertical );
+  g->layout()->setSpacing( 6 );
+  g->layout()->setMargin( 11 );
+  QGridLayout * v = new QGridLayout( g->layout() );
+  v->setAlignment( Qt::AlignTop );
+
+  rb0 = new QRadioButton(i18n("Vocabulary List"), g );
+  v->addWidget( rb0, 0, 0 );
+  rb1 = new QRadioButton(i18n("Vocabulary Exam"), g);
+  v->addWidget( rb1, 1, 0 );
+  rb2 = new QRadioButton(i18n("Flashcards"), g);
+  v->addWidget( rb2, 2, 0 );
+  l->addWidget( g, 0, 0 );
+}
+
+
+WQPrintDialogPage::~WQPrintDialogPage()
+{
+}
+
+void WQPrintDialogPage::getOptions( QMap< QString, QString > & opts, bool incldef )
+{
+  opts[ "kde-kwordquiz-type" ] = QString::number(g->selectedId());
+}
+
+void WQPrintDialogPage::setOptions( const QMap< QString, QString > & opts )
+{
+  QString type = opts[ "kde-kwordquiz-type" ];
+  switch (type.toInt())
+  {
+    case 0: rb0->setChecked(true); break;
+    case 1: rb1->setChecked(true); break;
+    case 2: rb2->setChecked(true); break;
+  }
+}
+
+bool WQPrintDialogPage::isValid( QString & msg )
+{
+  return true;
+}
+
+
+#include "wqprintdialogpage.moc"
---
+++ src/wqprintdialogpage.h
@@ -0,0 +1,44 @@
+//
+// C++ Interface: wqprintdialogpage
+//
+// Description: 
+//
+//
+// Author: Peter Hedlund <peter@peterandlinda.com>, (C) 2004
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef WQPRINTDIALOGPAGE_H
+#define WQPRINTDIALOGPAGE_H
+
+#include <qbuttongroup.h>
+#include <qradiobutton.h>
+
+#include <kdeprint/kprintdialogpage.h>
+
+/**
+Print dialog page to provide specific print options for KWordQuiz
+
+@author Peter Hedlund
+*/
+class WQPrintDialogPage : public KPrintDialogPage
+{
+Q_OBJECT
+public:
+    WQPrintDialogPage( QWidget *parent = 0, const char *name = 0 );
+
+    ~WQPrintDialogPage();
+   //reimplement virtual functions
+   void getOptions( QMap<QString,QString>& opts, bool incldef = false );
+   void setOptions( const QMap<QString,QString>& opts );
+   bool isValid( QString& msg );
+
+private:
+  QButtonGroup * g;
+  QRadioButton * rb0;
+  QRadioButton * rb1;
+  QRadioButton * rb2;       
+};
+
+#endif
