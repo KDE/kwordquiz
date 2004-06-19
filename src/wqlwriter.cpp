df6ac23157a0420464f8819a329605244df92148
Peter Hedlund <peter@peterandlinda.com>
2004-06-19 22:26:29 +0000

    updated license header
    svn path=/trunk/kdeedu/kwordquiz/; revision=322061

--- src/wqlwriter.cpp
+++ src/wqlwriter.cpp
@@ -1,14 +1,18 @@
-//
-// C<<<< Implementation: wqlwriter
-//
-// Description: 
-//
-//
-// Author: Peter Hedlund <peter@peterandlinda.com>, (C) 2004
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
+/***************************************************************************
+                          wqlwriter.cpp  -  description
+                             -------------------
+   copyright            : (C) 2004 by Peter Hedlund
+    email                : peter@peterandlinda.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 #include "wqlwriter.h"
 
 #define winendl "\r\n"
