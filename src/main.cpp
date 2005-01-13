c40b53a10f56235a9c6f633d7fc35802a948fb83
Peter Hedlund <peter@peterandlinda.com>
2005-01-13 23:29:30 +0000

    Added command line options to start directly with a quiz. Based on a patch by CPH. Thank you. CCMAIL:CP.Hennessy@openapp.biz
    svn path=/trunk/kdeedu/kwordquiz/; revision=378304

--- src/main.cpp
+++ src/main.cpp
@@ -25,22 +25,25 @@ static const char *description = I18N_NOOP("A powerful flashcard and vocabulary 
 
 static KCmdLineOptions options[] =
 {
+  { "m", 0, 0 },
+  { "mode <number>", I18N_NOOP("A number 1-5 corresponding to the \nentries in the Mode menu"), 0 },
+  { "g", 0, 0 },
+  { "goto <session>", I18N_NOOP("Type of session to start with: \n'flash' for flashcard, \n'mc' for multiple choice, \n'qa' for question and answer"), 0 },
   { "+[File]", I18N_NOOP("File to open"), 0 },
   { 0, 0, 0 }
-  // INSERT YOUR COMMANDLINE OPTIONS HERE
 };
 
 int main(int argc, char *argv[])
 {
-  KAboutData aboutData( "kwordquiz", I18N_NOOP("KWordQuiz"), KWQ_VERSION, description, KAboutData::License_GPL, "(c) 2003-2004, Peter Hedlund", 0, 0, "peter@peterandlinda.com");
-  aboutData.addAuthor("Peter Hedlund",0, "peter@peterandlinda.com");
+  KAboutData aboutData( "kwordquiz", I18N_NOOP("KWordQuiz"), KWQ_VERSION, description, KAboutData::License_GPL, "(c) 2003-2005, Peter Hedlund", 0, 0, "peter@peterandlinda.com");
+  aboutData.addAuthor("Peter Hedlund", 0, "peter@peterandlinda.com");
   aboutData.addCredit("Anne-Marie Mahfouf", I18N_NOOP("KDE Edutainment Maintainer"), "annma@kde.org", 0);
-  
+
   KCmdLineArgs::init( argc, argv, &aboutData );
   KCmdLineArgs::addCmdLineOptions( options ); // Add our own options.
 
   KApplication app;
- 
+
   if (app.isRestored())
   {
     RESTORE(KWordQuizApp);
@@ -54,7 +57,31 @@ int main(int argc, char *argv[])
 
     if (args->count())
     {
-        kwordquiz->openDocumentFile(args->arg(0));
+      kwordquiz->openDocumentFile(args->arg(args->count() - 1));
+      QCString mode = args->getOption("mode");
+      if (!mode.isEmpty())
+      {
+        if (mode == "1")
+          kwordquiz->slotMode1();
+        if (mode == "2")
+          kwordquiz->slotMode2();
+        if (mode == "3")
+          kwordquiz->slotMode3();
+        if (mode == "4")
+          kwordquiz->slotMode4();
+        if (mode == "5")
+          kwordquiz->slotMode5();
+      }
+      QCString go_to = args->getOption("goto");
+      if (!go_to.isEmpty())
+      {
+        if (go_to == "flash")
+          kwordquiz->slotQuizFlash();
+        if (go_to == "multiple")
+          kwordquiz->slotQuizMultiple();
+        if (go_to == "qa")
+          kwordquiz->slotQuizQA();
+      }
     }
     else
     {
@@ -63,4 +90,4 @@ int main(int argc, char *argv[])
     args->clear();
   }
   return app.exec();
-}  
+}
