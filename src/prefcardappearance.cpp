fe4fdc2d528952dde57ac3282a52e07bb8248bc4
Peter Hedlund <peter@peterandlinda.com>
2006-10-27 21:24:12 +0000

    Make the flashcard designer in the configuration dialog work again.
    svn path=/trunk/KDE/kdeedu/kwordquiz/; revision=599612

--- src/prefcardappearance.cpp
+++ src/prefcardappearance.cpp
@@ -28,6 +28,10 @@ PrefCardAppearance::PrefCardAppearance(QWidget *parent) : QWidget(parent)
   connect(kcfg_FrontTextColor, SIGNAL(changed(const QColor&)), this, SLOT(slotTextColorChanged(const QColor&)));
   connect(kcfg_FrontCardColor, SIGNAL(changed(const QColor&)), this, SLOT(slotCardColorChanged(const QColor&)));
   connect(kcfg_FrontFrameColor, SIGNAL(changed(const QColor&)), this, SLOT(slotFrameColorChanged(const QColor&)));
+  connect(kcfg_BackFont, SIGNAL(fontSelected(const QFont&)), this, SLOT(slotFontChanged(const QFont&)));
+  connect(kcfg_BackTextColor, SIGNAL(changed(const QColor&)), this, SLOT(slotTextColorChanged(const QColor&)));
+  connect(kcfg_BackCardColor, SIGNAL(changed(const QColor&)), this, SLOT(slotCardColorChanged(const QColor&)));
+  connect(kcfg_BackFrameColor, SIGNAL(changed(const QColor&)), this, SLOT(slotFrameColorChanged(const QColor&)));
   connect(widgetStack, SIGNAL(currentChanged(int)), this, SLOT(slotCurrentChanged(int)));
 
   widgetStack->setCurrentWidget(frontStackPage);
@@ -62,15 +66,15 @@ void PrefCardAppearance::slotCardColorChanged( const QColor & color)
   QPalette pal = cardFrame->palette();
   pal.setColor(cardFrame->backgroundRole(), color);
   cardFrame->setPalette(pal);
-  
+
   pal = titleLabel->palette();
   pal.setColor(titleLabel->backgroundRole(), color);
   titleLabel->setPalette(pal);
-  
+
   pal = textLabel->palette();
   pal.setColor(textLabel->backgroundRole(), color);
   textLabel->setPalette(pal);
-  
+
   pal = line->palette();
   pal.setColor(line->backgroundRole(), color);
   line->setPalette(pal);
@@ -81,7 +85,7 @@ void PrefCardAppearance::slotFrameColorChanged( const QColor & color)
   QPalette pal = cardFrame->palette();
   pal.setColor(cardFrame->foregroundRole(), color);
   cardFrame->setPalette(pal);
-  
+
   pal = line->palette();
   pal.setColor(line->foregroundRole(), color);
   line->setPalette(pal);
@@ -95,25 +99,25 @@ void PrefCardAppearance::slotCurrentChanged(int index)
     QPalette pal = titleLabel->palette();
     pal.setColor(titleLabel->backgroundRole(), kcfg_BackCardColor->color());
     titleLabel->setPalette(pal);
-    
+
     textLabel->setText(i18n("Answer"));
     textLabel->setFont(kcfg_BackFont->font());
-    
+
     pal = textLabel->palette();
     pal.setColor(textLabel->foregroundRole(), kcfg_BackTextColor->color());
     pal.setColor(textLabel->backgroundRole(), kcfg_BackCardColor->color());
     textLabel->setPalette(pal);
-    
+
     pal = cardFrame->palette();
     pal.setColor(cardFrame->foregroundRole(), kcfg_BackFrameColor->color());
     pal.setColor(cardFrame->backgroundRole(), kcfg_BackCardColor->color());
     cardFrame->setPalette(pal);
-    
+
     pal = line->palette();
     pal.setColor(line->foregroundRole(), kcfg_BackFrameColor->color());
     pal.setColor(line->backgroundRole(), kcfg_BackCardColor->color());
     line->setPalette(pal);
-    
+
     fontLabel->setBuddy(kcfg_BackFont);
     textColorLabel->setBuddy(kcfg_BackTextColor);
     frameColorLabel->setBuddy(kcfg_BackFrameColor);
@@ -125,31 +129,30 @@ void PrefCardAppearance::slotCurrentChanged(int index)
     QPalette pal = titleLabel->palette();
     pal.setColor(titleLabel->backgroundRole(), kcfg_FrontCardColor->color());
     titleLabel->setPalette(pal);
-    
+
     textLabel->setText(i18n("Question"));
     textLabel->setFont(kcfg_FrontFont->font());
-    
+
     pal = textLabel->palette();
     pal.setColor(textLabel->foregroundRole(), kcfg_FrontTextColor->color());
     pal.setColor(textLabel->backgroundRole(), kcfg_FrontCardColor->color());
     textLabel->setPalette(pal);
-    
+
     pal = cardFrame->palette();
     pal.setColor(cardFrame->foregroundRole(), kcfg_FrontFrameColor->color());
     pal.setColor(cardFrame->backgroundRole(), kcfg_FrontCardColor->color());
     cardFrame->setPalette(pal);
-    
+
     pal = line->palette();
     pal.setColor(line->foregroundRole(), kcfg_FrontFrameColor->color());
     pal.setColor(line->backgroundRole(), kcfg_FrontCardColor->color());
     line->setPalette(pal);
-    
+
     fontLabel->setBuddy(kcfg_FrontFont);
     textColorLabel->setBuddy(kcfg_FrontTextColor);
     frameColorLabel->setBuddy(kcfg_FrontFrameColor);
     cardColorLabel->setBuddy(kcfg_FrontCardColor);
   }
 }
-
 
 #include "prefcardappearance.moc"
